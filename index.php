ebdc31d3aa0d1f62260c71788c04518281bc08bc
Troels Ugilt Jensen <tujensen@gmail.com>
2014-03-06 21:15:12 +0100

    Changed text

--- index.php
+++ index.php
@@ -17,9 +17,14 @@ $db = new IIFdb();
 <body>
     <div class="centerdiv">
         <div class="toptext">
-            <p>
-              På denne side finder du to digte under udarbejdelse. Digtene bliver skrevet fortløbende af et kollektiv bestående af netjournalister hos dr.dk, tv2.dk, eb.dk, b.dk, bt.dk, jp.dk, information.dk og pol.dk. Digtene bliver redigeret af en robot. Robotten opsamler alle sidernes overskrifter, der starter med "Vi" og "Jeg". Disse overskrifter sætter den sammen til to digte. Digtene vil således blive længere, så længe de respektive sider bliver ved med at fodre robotten med overskrifter. Digtene opdateres hver time med de linjer, som de respektive nyhedskanaler har produceret i mellemtiden. Eneste redigeringsfrihed, robotten tager sig, er at fjerne alt hvad der måtte stå før et kolon, så vi'et og jeg'et bliver isoleret fra deres oprindelige afsendere og således kommer til at fremstå som fællessubjekter. Skulle man ønske at efterprøve linjernes autenticitet, så kan man holde musen over en linje for at se kilden. Vi takker den danske journaliststand for deres poetiske udladninger og glæder os til at følge deres fortsatte afsøgning af begreberne: individ og fællesskab.
-            </p>
+            <p>På denne side finder du to digte under udarbejdelse. Digtene bliver skrevet fortløbende af et kollektiv bestående af netjournalister hos dr.dk, tv2.dk, eb.dk, b.dk, bt.dk, jp.dk, information.dk og pol.dk. Digtene bliver redigeret af en robot. Robotten afsøger løbende nyhedsmediernes RSS-feeds, og udvælger alle de overskrifter, der starter med "Vi" og "Jeg". Disse linjer skriver den til i toppen af nedenstående "Vi"- og "Jeg"-digte. Digtene vil således blive længere, så længe de respektive nyhedsmedier bliver ved med at fodre robotten med overskrifter. Digtene opdateres hvert 10. min. med de overskrifter, som de respektive nyhedskanaler har produceret i mellemtiden. Skulle man ønske at efterprøve linjernes autenticitet, så kan man holde musen over en linje for at se kilden.</p>
+            <p>Der er således ingen menneskelig redigering. Robotten derimod tager sig tre redigeringsfriheder:</p>
+            <ol>
+              <li>Den fjerner alt hvad der måtte stå før et kolon, så Vi'et og Jeg'et bliver isoleret fra deres oprindelige afsendere. Eks. " Miley Cyrus: Jeg er enten kedelig eller en luder" bliver i digtet reduceret til: "Jeg er enten kedelig eller en luder". Således får Vi'et og Jeg'et lov at stå som fællessubjekter.</li>
+              <li>Robotten går udenom al sport, da sportsartikler indeholder mange egennavne, og da sportsartikler oftest handler om hold og spilleres forventninger i forhold til at slå andre hold og spillere. Hvilket resulterer i nogle forholdsvis uinteressante udsagn i konteksten.</li>
+              <li>Grundet nyhedsmediernes flittige brug af Ritzau, Reuters og des lige, så tjekker robotten om den allerede har skrevet den pågældende linje til indenfor de sidste 24 timer. Hvis det er tilfældet, så gentager den ikke. Således bliver Vi'et og Jeg'et konstaterende karakterer.</li>
+            </ol>
+            <p>En tak skal lyde til den danske journaliststand for deres poetiske udladninger. Vi glæder os til at følge jeres fortsatte afsøgning af begreberne: Individ og fællesskab.</p>
             <div class="bytext">- Christoffer Ugilt Jensen og Troels Ugilt Jensen</div>
         </div>
         <div class="textfield" >
