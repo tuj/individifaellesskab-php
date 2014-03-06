384633b7f722ec11d85b553388a3227572fc7065
Troels Ugilt Jensen <tujensen@gmail.com>
2014-03-06 19:54:45 +0100

    Changed path

--- db/iif_db.php
+++ db/iif_db.php
@@ -45,7 +45,7 @@ class IIFdb {
     $iItemsDB = $query->fetchAll(PDO::FETCH_ASSOC);
 
     // Get links to feeds.
-    $strFeeds = file_get_contents("./feeds.txt", true);
+    $strFeeds = file_get_contents("feeds.txt", false);
     $feeds = explode("\r", $strFeeds);
 
     // Added items go into this array
