5e1f4c87e6a2112719fc46d09f3e6c5d059c842d
Troels Ugilt Jensen <tujensen@gmail.com>
2014-03-08 20:16:00 +0100

    switch to curl

--- db/iif_db.php
+++ db/iif_db.php
@@ -58,7 +58,17 @@ class IIFdb {
     // Check for new content in each feed.
     foreach ($feeds as $feed) {
       if (Helpers::startsWith($feed, "http")) {
-        $feedContent = file_get_contents($feed);
+        // Get cURL resource
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+          CURLOPT_RETURNTRANSFER => 1,
+          CURLOPT_URL => $feed
+        ));
+        $feedContent = curl_exec($curl);
+        curl_close($curl);
+
+
+        //$feedContent = file_get_contents($feed);
         if ($xml = simplexml_load_string($feedContent)) {
           echo "Processing: " . $feed . "\n";
           foreach ($xml->xpath("//item") as $item) {
